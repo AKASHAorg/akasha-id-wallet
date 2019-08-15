400a1c81d1072344ae696377bc01685ee99c99a5
Andrei Sambra <andrei.sambra@akasha.world>
2019-08-15 15:38:50 +0300

    basic class definitions

--- src/index.js
+++ src/index.js
@@ -23,8 +23,13 @@ class DIDclient {
 
     // Initialize the signalhub connection
     async initHub (channel, callback) {
-      const hub = signalhub(channel, this.config.hubUrls)  
+      const hub = signalhub(channel, this.config.hubUrls)
+      // subscribe to the provided channel
       hub.subscribe(channel).on('data', callback)
+      // catch errors
+      hub.on('error', ({ url, error }) => {
+        throw(new Error('Websocket connection error', url, error))
+      })
       return hub
     }
     // Generate a special link to request access to the user's DID
@@ -32,8 +37,8 @@ class DIDclient {
       // generate a one time channel ID
       this.loginChannel = generateId()
       // generate a one time symmetric encryption key and reveal it to AKASHA.id
-      this.loginKey = await crypto.genAESKey(true, 'AES-GCM', 128)
-      const extractedKey = await crypto.exportKey(this.loginKey)
+      this.bootstrapKey = await crypto.genAESKey(true, 'AES-GCM', 128)
+      const extractedKey = await crypto.exportKey(this.bootstrapKey)
       const b64Key = Buffer.from(extractedKey).toString('base64')
       // use the wallet app URL for the link
       const loginUrl = new URL(this.config.walletBaseUrl)
@@ -43,6 +48,36 @@ class DIDclient {
   
       this.loginLink = loginUrl.href
       return this.loginLink
+    }
+
+    async bootstrapNewLogin () {
+      await this.genLoginLink()
+      try {
+        this.initHub(this.loginChannel, (data) => {
+          const msg = await crypto.decrypt(this.bootstrapKey, JSON.parse(data), 'base64')
+          switch (msg.status) {
+            case 'allowed':
+              this.acceptedLogin(msg.claim)
+              break
+            case 'denied':
+              this.rejectedLogin()
+              break
+            default:
+              throw new Error(`Unexpectedly received message with status ${msg.status}`)
+          }
+        })
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    async acceptedLogin (claim) {
+      console.log('Login accepted', claim)
+      this.claim = claim
+    }
+
+    async rejectedLogin () {
+      console.log('Login rejected')
     }
 }
 
