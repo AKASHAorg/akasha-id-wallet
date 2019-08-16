7d6196498c02dac008d9aca13962782dce97d1d8
Andrei Sambra <andrei.sambra@akasha.world>
2019-08-16 09:58:28 +0300

    add reject handler

--- test/app.js
+++ test/app.js
@@ -3,7 +3,7 @@ const acceptBtn = document.getElementById('accept')
 const rejectBtn = document.getElementById('reject')
 
 let client, wallet
-const profileClaim = {
+const testClaim = {
     "@context": ["https://www.w3.org/2018/credentials/v1", "https://schema.org/"],
     "type": ["VerifiableCredential", "IdentityCredential"],
     "issuer": "did:akasha:b0a3ca3d0126c4f4a951a46246de0712b7637b8f",
@@ -27,7 +27,11 @@ loginBtn.addEventListener('click', async () => {
     const link = await client.genLoginLink()
     document.getElementById('link').innerText = link
     try {
-        await client.bootstrapNewLogin((response) => {
+        await client.bootstrapNewLogin((response) => { // success
+            console.log(response)
+            document.getElementById('claim').innerText = JSON.stringify(response,null,2)
+        },
+        (response) => { // fail
             console.log(response)
             document.getElementById('claim').innerText = JSON.stringify(response,null,2)
         })
@@ -38,7 +42,12 @@ loginBtn.addEventListener('click', async () => {
 
 acceptBtn.addEventListener('click', async () => {
     wallet = new AKASHAid.DIDwallet()
+    const req = wallet.parseLoginLink(document.getElementById('request').value)  
+    await wallet.respondToLogin(req[2], req[1], testClaim, 'allowed')
+}, false)
+
+rejectBtn.addEventListener('click', async () => {
+    wallet = new AKASHAid.DIDwallet()
     const req = wallet.parseLoginLink(document.getElementById('request').value)
-    console.log(req)
-    await wallet.respondToLogin(req[2], req[1], profileClaim, 'allowed')
+    await wallet.respondToLogin(req[2], req[1], null, 'denied')
 }, false)
