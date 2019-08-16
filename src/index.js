85c8e18b2114d0d5dfd2d1509cf86fae9fa3c703
Andrei Sambra <andrei.sambra@akasha.world>
2019-08-16 12:51:18 +0300

    using standardjs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const signalhub = require('signalhub')
+const signalhub = require('signalhub') // might switch to SocketCluster later
 const generateId = require('./utils').generateId
 const crypto = require('masq-common').crypto
 
-const HUB_URLS = [ 'localhost:8080' ]
+const HUB_URLS = ['localhost:8080']
 const WALLET_URL = 'http://localhost:3000'
 
 let DEBUGGING = false
 
-function debug() {
-  if ( DEBUGGING ) {
-      console.log.apply(this, arguments);
+function debug () {
+  if (DEBUGGING) {
+    console.log.apply(this, arguments)
   }
 }
 
@@ -18,102 +18,101 @@ const initHub = (channel, hubUrls) => {
   const hub = signalhub(channel, hubUrls)
   // catch errors
   hub.on('error', ({ url, error }) => {
-    throw(new Error('Websocket connection error', url, error))
+    throw (new Error('Websocket connection error', url, error))
   })
   return hub
 }
 
 class DIDclient {
-    constructor (appName, appDescription, appImageURL, appURL, options = {}) {
-      this.appName = appName
-      this.appDescription = appDescription
-      this.appImageURL = appImageURL
-      this.appURL = appURL
-      
-      // init config
-      this.config = {
-        hubUrls: options.hubUrls ? options.hubUrls : HUB_URLS,
-        walletUrl: options.walletUrl ? options.walletUrl : WALLET_URL
-      }
-      // debug
-      DEBUGGING = options.debug ? options.debug : false
+  constructor (appName, appDescription, appImageURL, appURL, options = {}) {
+    this.appName = appName
+    this.appDescription = appDescription
+    this.appImageURL = appImageURL
+    this.appURL = appURL
+    // init config
+    this.config = {
+      hubUrls: options.hubUrls ? options.hubUrls : HUB_URLS,
+      walletUrl: options.walletUrl ? options.walletUrl : WALLET_URL
+    }
+    // debug
+    DEBUGGING = options.debug ? options.debug : false
+  }
+
+  // Generate a special link to request access to the user's DID
+  async genLoginLink () {
+    // generate a one time channel ID
+    this.loginChannel = generateId()
+    // generate NONCE
+    this.nonce = this.genNonce()
+    // generate a one time symmetric encryption key and reveal it to AKASHA.id
+    this.bootstrapKey = await crypto.genAESKey(true, 'AES-GCM', 128)
+    const extractedKey = await crypto.exportKey(this.bootstrapKey)
+    const b64Key = Buffer.from(extractedKey).toString('base64')
+    // use the wallet app URL for the link
+    const loginUrl = new URL(this.config.walletUrl)
+    const hashParams = JSON.stringify([this.appName, this.loginChannel, b64Key, this.nonce])
+    loginUrl.hash = '/link/' + Buffer.from(hashParams).toString('base64')
+
+    this.loginLink = loginUrl.href
+    debug('Generated link from:', hashParams)
+    return this.loginLink
+  }
+
+  // Generate a none
+  genNonce (min, max) {
+    min = Math.ceil(min || 100000)
+    max = Math.floor(max || 9999999)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  /*  Bootstrap the login process.
+    *  @param {function} success Function to call when the process succeeds
+    *  @param {function} fail Function to call when the process fails
+    */
+  async bootstrapNewLogin (success, fail) {
+    if (!this.loginLink) {
+      await this.genLoginLink()
     }
 
-    // Generate a special link to request access to the user's DID
-    async genLoginLink () {
-      // generate a one time channel ID
-      this.loginChannel = generateId()
-      // generate NONCE
-      this.nonce = this.genNonce()
-      // generate a one time symmetric encryption key and reveal it to AKASHA.id
-      this.bootstrapKey = await crypto.genAESKey(true, 'AES-GCM', 128)
-      const extractedKey = await crypto.exportKey(this.bootstrapKey)
-      const b64Key = Buffer.from(extractedKey).toString('base64')
-      // use the wallet app URL for the link
-      const loginUrl = new URL(this.config.walletUrl)
-      const hashParams = JSON.stringify([this.appName, this.loginChannel, b64Key, this.nonce])
-      loginUrl.hash = '/link/' + Buffer.from(hashParams).toString('base64')
-  
-      this.loginLink = loginUrl.href
-      debug('Generated link from:', hashParams)
-      return this.loginLink
-    }
-
-    // Generate a none
-    genNonce (min, max) {
-      min = Math.ceil(min || 100000)
-      max = Math.floor(max || 9999999)
-      return Math.floor(Math.random() * (max - min + 1)) + min
-    }
-
-    /*  Bootstrap the login process.
-     *  @param {function} success Function to call when the process succeeds
-     *  @param {function} fail Function to call when the process fails
-     */
-    async bootstrapNewLogin (success, fail) {
-      if (!this.loginLink) {
-        await this.genLoginLink()
-      }
-
-      try {
-        const hub = initHub(this.loginChannel, this.config.hubUrls)
-        hub.subscribe(this.loginChannel).on('data', async (data) => {
-          const msg = await crypto.decrypt(this.bootstrapKey, JSON.parse(data), 'base64')
-          if (msg.nonce && msg.nonce === this.nonce) {
-            switch (msg.status) {
-              case 'allowed':
-                success(msg)
-                break
-              case 'denied':
-                fail(msg)
-                break
-              default:
-                throw new Error(`Unexpectedly received message with status ${msg.status}`)
-            }
-            this.cleanUp(hub) 
-          } else {
-            debug('Got msg:', msg)
+    try {
+      const hub = initHub(this.loginChannel, this.config.hubUrls)
+      hub.subscribe(this.loginChannel).on('data', async (data) => {
+        const msg = await crypto.decrypt(this.bootstrapKey, JSON.parse(data), 'base64')
+        if (msg.nonce && msg.nonce === this.nonce) {
+          switch (msg.status) {
+            case 'allowed':
+              success(msg)
+              break
+            case 'denied':
+              fail(msg)
+              break
+            default:
+              throw new Error(`Unexpectedly received message with status ${msg.status}`)
           }
-        })
-      } catch (e) {
-        console.error(e)
-      }
+          this.cleanUp(hub)
+        } else {
+          debug('Got msg:', msg)
+        }
+      })
+    } catch (e) {
+      console.error(e)
     }
+  }
 
-    // Clean up the current request state and close the hub connection
-    cleanUp (hub) {
-      this.loginChannel = null
-      this.loginLink = null
-      this.nonce = null
-      hub.close()
-    }
+  // Clean up the current request state and close the hub connection
+  cleanUp (hub) {
+    this.loginChannel = null
+    this.loginLink = null
+    this.nonce = null
+    hub.close()
+  }
 }
 
 class DIDwallet {
   constructor (hubUrls) {
     // init config
     this.config = {
-      hubUrls: hubUrls ? hubUrls : HUB_URLS
+      hubUrls: hubUrls || HUB_URLS
     }
     this.did = `did:akasha:${generateId()}`
   }
@@ -129,12 +128,12 @@ class DIDwallet {
     }
   }
 
-  async respondToLogin (channel, rawKey, nonce, claim, status) {
+  async handleLogin (channel, rawKey, nonce, claim, status) {
     // init Websocket hub connection
     const hub = initHub(channel, this.config.hubUrls)
     // encrypt payload
     const key = await crypto.importKey(Buffer.from(rawKey, 'base64'))
-    const encryptedMsg = await crypto.encrypt(key, {status, claim, nonce}, 'base64')
+    const encryptedMsg = await crypto.encrypt(key, { status, claim, nonce }, 'base64')
 
     hub.broadcast(channel, JSON.stringify(encryptedMsg))
     this.cleanUp(hub)
--- test/app.js
+++ test/app.js
@@ -3,53 +3,53 @@ const acceptBtn = document.getElementById('accept')
 const rejectBtn = document.getElementById('reject')
 
 const testClaim = {
-    "@context": ["https://www.w3.org/2018/credentials/v1", "https://schema.org/"],
-    "type": ["VerifiableCredential", "IdentityCredential"],
-    "issuer": "did:akasha:b0a3ca3d0126c4f4a951a46246de0712b7637b8f",
-    "issuanceDate": "2019-08-14T19:73:24Z",
-    "credentialSubject": {
-        "id": "did:akasha:b0a3ca3d0126c4f4a951a46246de0712b7637b8f",
-        "name": "J. Doe",
-        "address": {
-            "streetAddress": "10 Rue de Chose",
-            "postalCode": "98052",
-            "addressLocality": "Paris",
-            "addressCountry": "FR"
-        },
-        "birthDate": "1982-03-15"
+  '@context': ['https://www.w3.org/2018/credentials/v1', 'https://schema.org/'],
+  type: ['VerifiableCredential', 'IdentityCredential'],
+  issuer: 'did:akasha:b0a3ca3d0126c4f4a951a46246de0712b7637b8f',
+  issuanceDate: new Date().toISOString(),
+  credentialSubject: {
+    id: 'did:akasha:b0a3ca3d0126c4f4a951a46246de0712b7637b8f',
+    name: 'J. Doe',
+    address: {
+      streetAddress: '10 Rue de Chose',
+      postalCode: '98052',
+      addressLocality: 'Paris',
+      addressCountry: 'FR'
     },
-    "proof": {}
+    birthDate: '1982-03-15'
+  },
+  proof: {}
 }
 
 loginBtn.addEventListener('click', async () => {
-    const client = new AKASHAid.DIDclient('a','b','c','d', {debug: true})
-    const link = await client.genLoginLink()
-    document.getElementById('link').innerText = link
-    document.getElementById('request').value = link
-    try {
-        await client.bootstrapNewLogin((response) => { // success
-            console.log(response)
-            document.getElementById('claim').innerText = JSON.stringify(response,null,2)
-        },
-        (response) => { // fail
-            console.log(response)
-            document.getElementById('claim').innerText = JSON.stringify(response,null,2)
-        })
-    } catch (e) {
-        console.log(e)
-    }
+  const client = new AKASHAid.DIDclient('a', 'b', 'c', 'd', { debug: true })
+  const link = await client.genLoginLink()
+  document.getElementById('link').innerText = link
+  document.getElementById('request').value = link
+  try {
+    await client.bootstrapNewLogin((response) => { // success
+      console.log(response)
+      document.getElementById('claim').innerText = JSON.stringify(response, null, 2)
+    },
+    (response) => { // fail
+      console.log(response)
+      document.getElementById('claim').innerText = JSON.stringify(response, null, 2)
+    })
+  } catch (e) {
+    console.log(e)
+  }
 }, false)
 
 acceptBtn.addEventListener('click', async () => {
-    wallet = new AKASHAid.DIDwallet()
-    const str = document.getElementById('request').value.substring(29)
-    const req = wallet.parseLoginLink(str)
-    await wallet.respondToLogin(req[1], req[2], req[3], testClaim, 'allowed')
+  const wallet = new AKASHAid.DIDwallet()
+  const str = document.getElementById('request').value.substring(29)
+  const req = wallet.parseLoginLink(str)
+  await wallet.handleLogin(req[1], req[2], req[3], testClaim, 'allowed')
 }, false)
 
 rejectBtn.addEventListener('click', async () => {
-    const wallet = new AKASHAid.DIDwallet()
-    const str = document.getElementById('request').value.substring(29)
-    const req = wallet.parseLoginLink(str)
-    await wallet.respondToLogin(req[1], req[2], req[3], null, 'denied')
+  const wallet = new AKASHAid.DIDwallet()
+  const str = document.getElementById('request').value.substring(29)
+  const req = wallet.parseLoginLink(str)
+  await wallet.handleLogin(req[1], req[2], req[3], null, 'denied')
 }, false)
